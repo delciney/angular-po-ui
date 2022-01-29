36f0662af463edfb16ba3c4b20cfe50b60421fcc
Delciney <delciney07@gmail.com>
2022-01-29 13:10:43 -0300

    0007

--- front/src/app/components/button-group/button-group.component.ts
+++ front/src/app/components/button-group/button-group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PoButtonGroupItem } from '@po-ui/ng-components';
 @Component({
   selector: 'app-button-group',
   templateUrl: './button-group.component.html',
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonGroupComponent implements OnInit {
 
   constructor() { }
+
+  buttons: Array<PoButtonGroupItem> = [
+    { label: 'Button 1', action: this.action.bind(this) },
+    { label: 'Button 2', action: this.action.bind(this) }
+  ];
+
+  action(button: any) {
+    alert(`${button.label}`);
+  }
 
   ngOnInit(): void {
   }
